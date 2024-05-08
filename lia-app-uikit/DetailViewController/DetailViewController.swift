eeb8888759855ccef18d6b43f048bc8f1bf56156
FiHe98 <henriksson.filip1998@gmail.com>
2024-05-08 09:07:20 +0200

    fixed layout

--- lia-app-uikit/DetailViewController/DetailViewController.swift
+++ lia-app-uikit/DetailViewController/DetailViewController.swift
@@ -19,34 +19,49 @@ class DetailViewController: UIViewController {
     }
     
     private func setupUI() {
+        // Main Stack View
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.backgroundColor = .blue
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        
+        let innerStackView = UIStackView()
+        innerStackView.axis = .vertical
+        innerStackView.backgroundColor = .green
+        innerStackView.alignment = .fill
+        innerStackView.distribution = .fill
+        
         let imageView = UIImageView()
-        let titleLabel = UILabel()
-        titleLabel.text = header
-        let bodyTextView = UITextView()
-        view.backgroundColor = .white
-        
-        view.addSubview(imageView)
-        view.addSubview(titleLabel)
-        view.addSubview(bodyTextView)
-        
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "dogimage")
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.heightAnchor.constraint(lessThanOrEqualToConstant: 300).isActive = true
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "TEST TEST TEST TEST"
+        titleLabel.backgroundColor = .orange
+
+        
+        let bodyTextView = UITextView()
+        bodyTextView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis eros viverra, bibendum urna et, tincidunt odio. Aenean leo odio, finibus in mi in, tincidunt pharetra nunc. Sed sollicitudin eu orci non tempus. Nulla vitae auctor nisi. Donec laoreet, felis ut lobortis sodales, purus dui scelerisque enim, sit amet cursus ligula lectus quis leo. Aliquam ac turpis nec arcu congue pretium et a leo. Nulla quis lectus feugiat, porta elit id, finibus nisi. Proin sed ligula sed lacus scelerisque congue. Etiam a suscipit lorem. Suspendisse ultrices sem vitae tempor posuere. Duis auctor nibh in nisi pretium sollicitudin. Donec lacus dui, consectetur nec aliquet et, commodo vel arcu."
+        bodyTextView.backgroundColor = .systemPink
+        
+        innerStackView.addArrangedSubview(titleLabel)
+        innerStackView.addArrangedSubview(bodyTextView)
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(innerStackView)
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            imageView.heightAnchor.constraint(equalToConstant: 200),
-            
-            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
-            titleLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
-            
-            bodyTextView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-            bodyTextView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
-            bodyTextView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
-            bodyTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
